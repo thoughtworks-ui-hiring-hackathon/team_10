3dcae553a8a57d6ffc8b96d99416ce796674c5eb
wankhedeamol <wankhedeamol20@gmail.com>
2019-06-21 12:17:08 +0530

    Created service for Api call and added function for get movie details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from "../rest.service";
 
 @Component({
   selector: 'app-details',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private rest: RestService) { }
+  movieDetail: any;
   ngOnInit() {
+    this.rest.getDetails('https://api.themoviedb.org/3/movie/508?api_key=7b838346925313a3f9f3aec97733517c&language=en-US&append_to_response=credits').subscribe(
+      (response) => {
+        this.movieDetail = response;
+        console.log(response);
+      }, (err) => {
+        console.error(err);
+      }
+    );
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../rest.service'
 
 @Component({
   selector: 'app-home',
